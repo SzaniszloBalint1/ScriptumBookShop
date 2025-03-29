1464f4b8a1d19c175c616be52317930df9ab2f58
SzaniszloBalint1 <113982176+SzaniszloBalint1@users.noreply.github.com>
2025-03-29 16:36:38 +0100

    Create web.php

---
+++ Backend/routes/web.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\CategoryController;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Route;
+use Laravel\Sanctum\Http\Controllers\CsrfCookieController;
+
