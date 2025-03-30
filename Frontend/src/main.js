7e763871f2ef76beb63c3599c82e5a98e87c6cd5
SzaniszloBalint1 <113982176+SzaniszloBalint1@users.noreply.github.com>
2025-03-30 20:19:36 +0200

    Add files via upload

---
+++ Frontend/src/main.js
@@ -0,0 +1,13 @@
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router';
+import store from './vuex';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+const app=createApp(App)
+app.use(router)
+app.use(store)
+app.mount('#app');
