3b4e65d0c18334c41bf63f096f2b41ea6e169da5
SzaniszloBalint1 <113982176+SzaniszloBalint1@users.noreply.github.com>
2025-04-01 20:03:34 +0200

    Add files via upload

---
+++ Frontend/src/Uservuex/getters.js
@@ -0,0 +1,27 @@
+export default {
+        user(state){
+            return state.user;
+        },
+        hasuser(state){
+            return !!state.user;   
+        },
+        users(state) {
+            return state.users;
+          },
+        userId(state){
+            return state.userId;
+        },
+        role(state){
+            return state.user ? state.user.role : null;
+        },
+        emailStatus(state) {
+            return state.emailStatus;
+        },
+        email(state) {
+            return state.email;
+        },
+        emailStatus(state) {
+            return state.emailStatus;
+        },
+        
+}
