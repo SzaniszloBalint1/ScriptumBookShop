d095a584a9ba32f8658cef3f9717450bb01ac907
Zsolt Görtler <zsoltgortler@gmail.com>
2025-04-04 19:07:27 +0200

    Add files via upload

---
+++ Frontend/src/components/Cart/CartButton.vue
@@ -0,0 +1,16 @@
+<template>
+<button @click="CartPage"><slot></slot></button>
+</template>
+
+
+<script>
+export default{
+    methods:{
+        CartPage(){
+            return this.$router.push('/cart');
+        }
+    }
+}
+</script>
+
+
