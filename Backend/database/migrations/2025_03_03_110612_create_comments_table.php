8f33b5001ada9b3d53c55745e12152c042ff3639
Zsolt Görtler <zsoltgortler@gmail.com>
2025-04-17 20:08:19 +0200

    Add files via upload

--- Backend/database/migrations/2025_03_03_110612_create_comments_table.php
+++ Backend/database/migrations/2025_03_03_110612_create_comments_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('book_id')->constrained('books');
-            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('book_id')->constrained('books')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->text('comment');
             $table->timestamps();
         });
