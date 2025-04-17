3ad2146bb68da56ce11dd029737e481e30cc62e1
Zsolt Görtler <zsoltgortler@gmail.com>
2025-04-17 20:08:42 +0200

    Add files via upload

--- Backend/database/migrations/2025_02_27_112634_create_rating_table.php
+++ Backend/database/migrations/2025_02_27_112634_create_rating_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('rating', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('book_id')->constrained('books');
-            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('book_id')->constrained('books')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
          $table->integer('rating');
          $table->unique(['user_id', 'book_id']);
          $table->timestamps();
